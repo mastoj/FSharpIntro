80402638c42d9cb7def7b8a6067dc6c0e6072979
Tomas Jansson <tomas.jansson@gmail.com>
2017-04-07 22:35:04 +0200

    module 2 start

--- src/FSharpIntro/Script1.fsx
+++ src/FSharpIntro/Script1.fsx
@@ -1,1 +1,50 @@
-﻿
+﻿open System
+
+type CustomerId = int
+type [<Measure>] Point
+type Points = float<Point>
+type CustomerName = string
+
+type GoldData = 
+    {
+        PromotedDate: DateTime
+    }
+
+type FrequentFlyerStatus = 
+    | Regular
+    | Gold of GoldData
+
+type Customer = 
+    {
+        Id: CustomerId
+        Name: CustomerName
+        Points: Points
+        FrequentFlyerStatus: FrequentFlyerStatus
+    }
+
+type [<Measure>] Km
+type Distance = float<Km>
+
+type DestinationName = string
+type DestinationId = int
+type Destination = 
+    {
+        Id: int
+        Name: DestinationName
+    }
+
+type [<Measure>] SEK
+type Price = float<SEK>
+
+type TicketClass = 
+    | Economy
+    | Business
+    | FirstClass
+type Ticket = 
+    {
+        CustomerId: CustomerId
+        Price: Price
+        TicketClass: TicketClass
+        From: Destination
+        To: Destination
+    }
