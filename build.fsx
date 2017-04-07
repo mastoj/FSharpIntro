eaae0af7ff4ab67e7703a375819fbe01fe38adb6
Tomas Jansson <tomas.jansson@gmail.com>
2017-04-07 22:49:55 +0200

    correct name and repo

--- build.fsx
+++ build.fsx
@@ -9,10 +9,10 @@
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "myGitUser"
+let gitOwner = "mastoj"
 let gitHome = "https://github.com/" + gitOwner
 // The name of the project on GitHub
-let gitProjectName = "MyProject"
+let gitProjectName = "FSharpBucks"
 // The name of the GitHub repo subdirectory to publish slides to
 let gitSubDir = ""
 
