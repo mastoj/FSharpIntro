27605d815d860b0266a94a12a3d955d90412500e
Tomas Jansson <tomas.jansson@gmail.com>
2017-04-07 22:35:04 +0200

    module3 start

--- src/FSharpIntro/Script1.fsx
+++ src/FSharpIntro/Script1.fsx
@@ -48,3 +48,6 @@ type Ticket =
         From: Destination
         To: Destination
     }
+﻿
+﻿
+﻿#load "Types.fs
--- src/FSharpIntro/Types.fs
+++ src/FSharpIntro/Types.fs
@@ -1,3 +1,67 @@
 ﻿module Types
 open System
 
+type CustomerId = int
+type [<Measure>] Point
+type Points = float<Point>
+type CustomerName = string
+
+type GoldData = 
+    {
+        PromotedDate: DateTime
+    }
+
+type FrequentFlyerStatus = 
+    | Regular
+    | Gold of GoldData
+
+type Customer = 
+    {
+        Id: CustomerId
+        Name: CustomerName
+        Points: Points
+        FrequentFlyerStatus: FrequentFlyerStatus
+    }
+    with 
+        static member create id name = {Id = id; Name = name; Points = 0.<Point>; FrequentFlyerStatus = Regular}
+        member this.isGoldMember() = 
+            match this.FrequentFlyerStatus with
+            | Gold _ -> true
+            | Regular -> false
+
+type [<Measure>] Km
+type Distance = float<Km>
+
+type DestinationName = string
+type DestinationId = int
+type Destination = 
+    {
+        Id: int
+        Name: DestinationName
+    }
+    with static member create id name = {Id = id; Name = name}
+
+type [<Measure>] SEK
+type Price = float<SEK>
+
+type TicketClass = 
+    | Economy
+    | Business
+    | FirstClass
+type Ticket = 
+    {
+        CustomerId: CustomerId
+        Price: Price
+        TicketClass: TicketClass
+        From: Destination
+        To: Destination
+    }
+    with static member create id price ticketClass from toDest =
+        {
+            CustomerId = id
+            Price = price
+            TicketClass = ticketClass
+            From = from
+            To = toDest
+        }
+
